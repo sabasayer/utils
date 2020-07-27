38d577df8bc5694b555732fff7fac288207836b1
salihbaki <salihbakisayer@hotmail.com.tr>
2020-07-27 17:36:47 +0300

    prevent trigger on vertical scroll

--- src/dom/dom.util.ts
+++ src/dom/dom.util.ts
@@ -1,55 +1,59 @@
 import { PositionCalculaterUtil } from "../..";
 
 export abstract class DomUtil {
+  static findParentElement(
+    el: HTMLElement,
+    classValue?: string,
+    id?: string
+  ): HTMLElement | null {
+    if (!classValue && !id) return null;
 
-    static findParentElement(
-        el: HTMLElement,
-        classValue?: string,
-        id?: string
-    ): HTMLElement | null {
-        if (!classValue && !id) return null;
-
-        let parentEl = el.parentElement;
-        while (parentEl) {
-            if (
-                (id && parentEl.getAttribute("id") == id) ||
-                (classValue &&
-                    parentEl.classList &&
-                    parentEl.classList.contains(classValue))
-            ) {
-                break;
-            } else {
-                parentEl = parentEl.parentElement;
-            }
-        }
-
-        return parentEl;
+    let parentEl = el.parentElement;
+    while (parentEl) {
+      if (
+        (id && parentEl.getAttribute("id") == id) ||
+        (classValue &&
+          parentEl.classList &&
+          parentEl.classList.contains(classValue))
+      ) {
+        break;
+      } else {
+        parentEl = parentEl.parentElement;
+      }
     }
 
-    static randomColor() {
-        let r = Math.floor(Math.random() * 255);
-        let g = Math.floor(Math.random() * 255);
-        let b = Math.floor(Math.random() * 255);
+    return parentEl;
+  }
 
-        return `rgb(${r},${g},${b})`;
-    }
+  static randomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
 
-    static touchPositionInelement(event: TouchEvent, parent: HTMLElement) {
-        let canvasOffset = PositionCalculaterUtil.offset(parent);
-        let firstTouch = event.touches[0];
-        let x = firstTouch.clientX - canvasOffset.left;
-        let y = firstTouch.clientY - canvasOffset.top;
-        return { x, y };
-    }
+    return `rgb(${r},${g},${b})`;
+  }
 
-    static handleInfineteScroll(element: HTMLElement, callback: Function) {
-        element.addEventListener("scroll", e => {
-            let scrollTop = element.scrollTop;
-            let offsetHeight = element.offsetHeight;
-            let scrollHeight = element.scrollHeight;
-            if (offsetHeight + scrollTop >= scrollHeight) {
-                callback();
-            }
-        });
-    }
+  static touchPositionInelement(event: TouchEvent, parent: HTMLElement) {
+    let canvasOffset = PositionCalculaterUtil.offset(parent);
+    let firstTouch = event.touches[0];
+    let x = firstTouch.clientX - canvasOffset.left;
+    let y = firstTouch.clientY - canvasOffset.top;
+    return { x, y };
+  }
+
+  static handleInfineteScroll(element: HTMLElement, callback: Function) {
+    let prevScrollTop = 0;
+    element.addEventListener("scroll", (e) => {
+      let scrollTop = element.scrollTop;
+      let offsetHeight = element.offsetHeight;
+      let scrollHeight = element.scrollHeight;
+      if (
+        offsetHeight + scrollTop >= scrollHeight &&
+        scrollTop != prevScrollTop
+      ) {
+        callback();
+      }
+      prevScrollTop = scrollTop;
+    });
+  }
 }
